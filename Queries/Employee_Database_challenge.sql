6ed4f3cf42941777072cfb17d6ded906ffba3fda
CTPIM <114550235+CTPim@users.noreply.github.com>
2022-11-23 22:33:39 -0500

    challenge complete

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,115 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+CREATE TABLE employees (
+	 emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE titles(
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, title, from_date)
+);
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--SELECT * FROM retirement_info;
+
+SELECT employees.emp_no,
+     employees.first_name,
+	 employees.last_name,
+	 titles.title,
+	 titles.from_date,
+	 titles.to_date
+INTO retirement_titles
+FROM employees
+INNER JOIN titles
+ON (employees.emp_no = titles.emp_no)
+WHERE (employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+--SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (retirement_titles.emp_no) retirement_titles.emp_no,
+retirement_titles.first_name,
+retirement_titles.last_name,
+retirement_titles.title
+
+INTO unique_titles
+FROM retirement_titles
+WHERE (retirement_titles.to_date = '9999-01-01')
+ORDER BY emp_no ASC, to_date DESC;
+
+--SELECT * FROM unique_titles;
+
+--retrieve the number of employees by their most recent job title who are about to retire.
+
+SELECT COUNT(unique_titles.emp_no),unique_titles.title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+--write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+SELECT DISTINCT ON(employees.emp_no) employees.emp_no,
+employees.first_name,
+employees.last_name,
+employees.birth_date,
+dept_emp.from_date,
+dept_emp.to_date,
+titles.title
+INTO mentor_eligibility
+FROM employees
+INNER JOIN dept_emp
+ON (employees.emp_no = dept_emp.emp_no)
+INNER JOIN titles
+ON (employees.emp_no = titles.emp_no)
+WHERE (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (dept_emp.to_date = '9999-01-01')
+ORDER BY emp_no;
+
+--SELECT * FROM mentor_eligibility;
